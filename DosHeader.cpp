b05a70ec4d7734c1765494920adc8b2e4ba5f9e2
kairemUH <kairem@hawaii.edu>
2023-05-01 14:46:21 -1000

    Updated Dos header to use uints instead of strings

--- DosHeader.cpp
+++ DosHeader.cpp
@@ -12,41 +12,48 @@
 
 #include "DosHeader.hpp"
 
-DosHeader::DosHeader( char dosLine[64] ) {
+////////////////////  Constructors  ////////////////////
+DosHeader::DosHeader( uint8_t dosLine[64] ) {
 
-    std::string dos = dosLine;
+    // Store each value into its respective section.
+    memcpy(&e_magic,    dosLine,     2);
 
-    if( dos.substr(0,2).compare("ZM") ) {
-        //Error
+    if( e_magic != 0x4D5A ) {
+        //@todo print error
     }
-    // Store each value into its respective char array as big endian bytes. Then can simply type cast to int if need to process.
-    e_magic     = dos.substr(0,2);   std::reverse(std::begin(e_magic), std::end(e_magic));
-    e_cblp      = dos.substr(2,2);   std::reverse(std::begin(e_cblp), std::end(e_cblp));
-    e_cp        = dos.substr(4,2);   std::reverse(std::begin(e_cp), std::end(e_cp));
-    e_crlc      = dos.substr(6,2);   std::reverse(std::begin(e_crlc), std::end(e_crlc));
-    e_cparhdr   = dos.substr(8,2);   std::reverse(std::begin(e_cparhdr), std::end(e_cparhdr));
-    e_minalloc  = dos.substr(10,2);  std::reverse(std::begin(e_minalloc), std::end(e_minalloc));
-    e_maxalloc  = dos.substr(12,2);  std::reverse(std::begin(e_maxalloc), std::end(e_maxalloc));
-    e_ss        = dos.substr(14,2);  std::reverse(std::begin(e_ss), std::end(e_ss));
-    e_sp        = dos.substr(16,2);  std::reverse(std::begin(e_sp), std::end(e_sp));
-    e_csum      = dos.substr(18,2);  std::reverse(std::begin(e_csum), std::end(e_csum));
-    e_ip        = dos.substr(20,2);  std::reverse(std::begin(e_ip), std::end(e_ip));
-    e_cs        = dos.substr(22,2);  std::reverse(std::begin(e_cs), std::end(e_cs));
-    e_lfarlc    = dos.substr(24,2);  std::reverse(std::begin(e_lfarlc), std::end(e_lfarlc));
-    e_ovno      = dos.substr(26,2);  std::reverse(std::begin(e_ovno), std::end(e_ovno));
-    e_res1      = dos.substr(28,8);  std::reverse(std::begin(e_res1), std::end(e_res1));
-    e_oemid     = dos.substr(36,2);  std::reverse(std::begin(e_oemid), std::end(e_oemid));
-    e_oeminfo   = dos.substr(38,2);  std::reverse(std::begin(e_oeminfo), std::end(e_oeminfo));
-    e_res2      = dos.substr(40,20); std::reverse(std::begin(e_res2), std::end(e_res2));
-    e_lfanew    = dos.substr(60,4);  std::reverse(std::begin(e_lfanew), std::end(e_lfanew));
+
+    memcpy(&e_cblp,     dosLine+2,   2);
+    memcpy(&e_cp,       dosLine+4,   2);
+    memcpy(&e_crlc,     dosLine+6,   2);
+    memcpy(&e_cparhdr,  dosLine+8,   2);
+    memcpy(&e_minalloc, dosLine+10,  2);
+    memcpy(&e_maxalloc, dosLine+12,  2);
+    memcpy(&e_ss,       dosLine+14,  2);
+    memcpy(&e_sp,       dosLine+16,  2);
+    memcpy(&e_csum,     dosLine+18,  2);
+    memcpy(&e_ip,       dosLine+20,  2);
+    memcpy(&e_cs,       dosLine+22,  2);
+    memcpy(&e_lfarlc,   dosLine+24,  2);
+    memcpy(&e_ovno,     dosLine+26,  2);
+    memcpy(&e_res1,     dosLine+28,  8);
+    memcpy(&e_oemid,    dosLine+36,  2);
+    memcpy(&e_oeminfo,  dosLine+38,  2);
+    memcpy(&e_res2,     dosLine+40, 20);
+    memcpy(&e_lfanew,   dosLine+60,  4);
 
 }
 
-/*
-
-for(i=0;i<strlen(str)/2;i++) {
-    swap(str[i], str[sizeof(str) - i - 1]);
+////////////////////  Getters and Printing  ////////////////////
+uint16_t DosHeader::getMagic() {
+    return DosHeader::e_magic;
 }
 
+uint32_t DosHeader::getElfanew() {
+    return DosHeader::e_lfanew;
+}
 
-*/
+void DosHeader::printDosHeaderInfo() {
+
+    
+
+}
--- DosHeader.hpp
+++ DosHeader.hpp
@@ -13,39 +13,40 @@
 #pragma once
 
 #include <iostream>
+#include <cassert>
 
 class DosHeader {
 
 ////////////////////  DOS Header Member Variables  ////////////////////
 private:
-    char    e_magic[2];              // Magic number. Should be set to 0x5A4D
-    char    e_cblp[2];               // Bytes on last page of file
-    char    e_cp[2];                 // Pages in file
-    char    e_crlc[2];               // Relocations
-    char    e_cparhdr[2];            // Size of header in paragraphs
-    char    e_minalloc[2];           // Minimum extra paragraphs needed
-    char    e_maxalloc[2];           // Maximum extra paragraphs needed
-    char    e_ss[2];                 // Initial (relative) SS value
-    char    e_sp[2];                 // Initial SP value
-    char    e_csum[2];               // Checksum
-    char    e_ip[2];                 // Initial IP value
-    char    e_cs[2];                 // Initial (relative) CS value
-    char    e_lfarlc[2];             // File address of relocation table
-    char    e_ovno[2];               // Overlay number
-    char    e_res1[8];               // Reserved words. 4 words at 2 bytes each
-    char    e_oemid[2];              // OEM identifier (for e_oeminfo)
-    char    e_oeminfo[2];            // OEM information; e_oemid specific
-    char    e_res2[20];              // Reserved words. 10 words at 2 bytes each
-    char    e_lfanew[4];             // File address of new exe header
+    uint16_t    e_magic;              // Magic number. Should be set to 'MZ'
+    uint16_t    e_cblp;               // Bytes on last page of file
+    uint16_t    e_cp;                 // Pages in file
+    uint16_t    e_crlc;               // Relocations
+    uint16_t    e_cparhdr;            // Size of header in paragraphs
+    uint16_t    e_minalloc;           // Minimum extra paragraphs needed
+    uint16_t    e_maxalloc;           // Maximum extra paragraphs needed
+    uint16_t    e_ss;                 // Initial (relative) SS value
+    uint16_t    e_sp;                 // Initial SP value
+    uint16_t    e_csum;               // Checksum
+    uint16_t    e_ip;                 // Initial IP value
+    uint16_t    e_cs;                 // Initial (relative) CS value
+    uint16_t    e_lfarlc;             // File address of relocation table
+    uint16_t    e_ovno;               // Overlay number
+    uint16_t    e_res1[4];            // Reserved words. 4 words at 2 bytes each
+    uint16_t    e_oemid;              // OEM identifier (for e_oeminfo)
+    uint16_t    e_oeminfo;            // OEM information; e_oemid specific
+    uint16_t    e_res2[10];           // Reserved words. 10 words at 2 bytes each
+    uint32_t    e_lfanew;             // File address of new exe header
 
 ////////////////////  Constructors  ////////////////////
 public:
-    DosHeader( char dosLine[64] );
+    DosHeader( uint8_t dosLine[64] );
 
 ////////////////////  Getters and Printing  ////////////////////
 public:
+    uint16_t getMagic();
+    uint32_t getElfanew();
     void printDosHeaderInfo();
-    char* get0x3c();
-
 
 };
