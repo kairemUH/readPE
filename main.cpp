add61962c7fa0b2c683dcfd495b0d62768a3c430
kairemUH <kairem@hawaii.edu>
2023-05-03 02:43:53 -1000

    Updated main to properly test dos header

--- main.cpp
+++ main.cpp
@@ -13,7 +13,54 @@
 #pragma once
 
 #include <iostream>
+#include <fstream>
+
+#include "DosHeader.hpp"
+
+using namespace std;
 
 int main( int argc, char* argv[] ) {
+
+    // @todo send all errors to stderr
+
+    //////////  Error checking  //////////
+
+    if( argc < 2 ) {
+
+        printf( "Error: No file specified.\n" );
+
+        exit(EXIT_FAILURE);
+
+    }
+
+    if( argc > 2 ) {
+
+        printf("Error: Too many arguments.\n");
+
+        exit(EXIT_FAILURE);
+
+    }
+
+    /// Open the PE file in read only mode
+    ifstream PEFile;
+
+    PEFile.open( argv[1], ios::in );
+
+    if( !PEFile ) {
+
+        cout << "Error: could not open file [maps]." << endl;
+
+        exit(EXIT_FAILURE);
+
+    }
+
+    uint8_t byteArray[64];
+
+    PEFile.read( (char*)byteArray, 64 );
+
+    DosHeader testHeader = DosHeader( byteArray );
+
+    testHeader.printDosHeaderInfo();
+
     return 0;
 }
