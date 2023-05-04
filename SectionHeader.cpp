0a980ea3b68cb0e20a4a64ac80248dda0fd7a30b
kairemUH <kairem@hawaii.edu>
2023-05-03 14:51:57 -1000

    Wrote out Section Header object

--- SectionHeader.cpp
+++ SectionHeader.cpp
@@ -9,3 +9,51 @@
 /// @file    PEfile.hpp
 /// @author  Kai Matsusaka <kairem@hawaii.edu>
 ///////////////////////////////////////////////////////////////////////////////
+
+#include "SectionHeader.hpp"
+
+SectionHeader::SectionHeader( uint8_t byteArray[40] ) {
+
+    memcpy( &name,                  byteArray,      8 );
+    memcpy( &virtualSize,           byteArray+8,    4 );
+    memcpy( &virtualAddress,        byteArray+12,   4 );
+    memcpy( &sizeOfRawData,         byteArray+16,   4 );
+    memcpy( &pointerToRawData,      byteArray+20,   4 );
+    memcpy( &pointerToRelocations,  byteArray+24,   4 );
+    memcpy( &pointerToLinenumbers,  byteArray+28,   4 );
+    memcpy( &numberOfRelocations,   byteArray+32,   2 );
+    memcpy( &numberOfLinenumbers,   byteArray+34,   2 );
+    memcpy( &characteristics,       byteArray+36,   4 );
+
+}
+
+void SectionHeader::printSectionHeaderInfo() {
+
+    char str[33];
+    printf( "    Section\n" );
+
+    std::strcpy(str, "Name:");
+    printf( "        %-33s%.8s\n", str, (char*)&name );
+
+    std::strcpy(str, "Virtual Size:");
+    printf( "        %-33s0x%x\n", str, virtualSize );         // @todo print out bytes for these
+
+    std::strcpy(str, "Virtual Address:");
+    printf( "        %-33s0x%x\n", str, virtualAddress );
+
+    std::strcpy(str, "Size Of Raw Data:");
+    printf( "        %-33s0x%x\n", str, sizeOfRawData );
+
+    std::strcpy(str, "Pointer To Raw Data:");
+    printf( "        %-33s0x%x\n", str, pointerToRawData );
+
+    std::strcpy(str, "Number Of Relocations:");
+    printf( "        %-33s%u\n", str, numberOfRelocations );
+
+    std::strcpy(str, "Characteristics:");
+    printf( "        %-33s0x%x\n", str, characteristics );
+
+    std::strcpy(str, "Characteristics Names:");
+    printf( "        %-33s\n", str );     // @todo print out characteristic names
+
+}
--- SectionHeader.hpp
+++ SectionHeader.hpp
@@ -12,3 +12,30 @@
 
 #pragma once
 
+#include <iostream>
+#include <cstring>
+
+class SectionHeader {
+
+////////////////////  Section Header Member Variables  ////////////////////
+private:
+    uint8_t     name[8];
+    uint32_t    virtualSize;
+    uint32_t    virtualAddress;
+    uint32_t    sizeOfRawData;
+    uint32_t    pointerToRawData;
+    uint32_t    pointerToRelocations;
+    uint32_t    pointerToLinenumbers;
+    uint16_t    numberOfRelocations;
+    uint16_t    numberOfLinenumbers;
+    uint32_t    characteristics;
+
+////////////////////  Constructors  ////////////////////
+public:
+    SectionHeader( uint8_t byteArray[40] );
+
+////////////////////  Getters and Printing  ////////////////////
+public:
+    void printSectionHeaderInfo();
+
+};
