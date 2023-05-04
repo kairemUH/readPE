5c7de1a44b0fef3ad945034ccc29717d6bc4dbb9
kairemUH <kairem@hawaii.edu>
2023-05-03 14:15:21 -1000

    Initial Commit

---
+++ SectionHeader.hpp
@@ -0,0 +1,14 @@
+///////////////////////////////////////////////////////////////////////////////
+///          University of Hawaii, College of Engineering
+///          Lab 6 - readpe - SRE - Spring 2023
+///
+///
+/// readpe - Read and display information about Portable Executable (PE) and Common Object File Format (COFF) files
+/// 
+///
+/// @file    PEfile.hpp
+/// @author  Kai Matsusaka <kairem@hawaii.edu>
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
